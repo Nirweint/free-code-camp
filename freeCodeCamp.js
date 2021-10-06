9daa972b0877c236608fa9c8cc551da77d1e61e3
Nirweint <nirweint_1@mail.ru>
2021-10-06 13:29:38 +0300

    13 fcc

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -101,3 +101,16 @@ let myHouse = new House(4);
 console.log(myHouse instanceof House);
 
 // 13
+function Bird(name) {
+	this.name = name;
+	this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+
+for (let property in canary) {
+	if(canary.hasOwnProperty(property)) {
+		ownProps.push(property);
+	}
+}
